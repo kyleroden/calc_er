30f76245f6b12f5e6a2eeb516c6552b69587e80d
kyleroden <kyleroden77@gmail.com>
2016-09-06 12:01:52 -0700

    first commit

---
+++ calc_er/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("#wrapper").css("background-color", "cornflowerblue");
+  $("#wrapper").append("<p>Jquery is loaded</p>");
+});
+
+var adder = function(num1, num2) {
+ return num1 + num2;
+}
+
+var subtractor = function(num1, num2) {
+  return num1 - num2;
+}
